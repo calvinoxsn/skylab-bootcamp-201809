a2011eebd67d6755cc4680e533447c963e058c4d
Tomás Sánchez Navarro <tsanchez.navarro@gmail.com>
2018-10-04 17:54:16 +0200

    replaced my safe-box with manu's safe-box

--- staff/tomas-sancheznavarro/safe-box/safe-box.js
+++ staff/tomas-sancheznavarro/safe-box/safe-box.js
@@ -1,33 +1,11 @@
-var safeBox;
+// safe-box.js
 
-(function () {
-    var secret;
-    var password;
+var safeBox = {
+    saveSecret: function(secret, password) {
+        // TODO
+    },
 
-    var safeBox = {
-        saveSecret: function (_secret, _password) {
-            // TODO
-            if (typeof _password === 'undefined' && typeof _password !== String) {
-                throw Error("Password must be set");
-            }
-            if (_password.length === 0) {
-                throw Error('invalid password');
-            }
-
-            if (!_password.trim().length === 0) {
-                throw Error('invalid password')
-            }
-
-            secret = _secret;
-            password = _password;
-        },
-        retrieveSecret: function (_password) {
-            // TODO
-            if (password === _password) {
-                return secret;
-            }
-            throw Error("Invalid password");
-        }
+    retrieveSecret: function(password) {
+        // TODO
     }
-    return safeBox;
-})()
+}
