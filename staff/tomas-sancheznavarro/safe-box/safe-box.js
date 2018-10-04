7859e24243975bcfc0ac1c0b3a194f24e0db7696
Tomás Sánchez Navarro <tsanchez.navarro@gmail.com>
2018-10-04 16:26:14 +0200

    added files for safebox challenge

---
+++ staff/tomas-sancheznavarro/safe-box/safe-box.js
@@ -0,0 +1,23 @@
+function passAdmin(_secret, _password) {
+    var secret;
+    var password;
+    var safeBox = {
+        saveSecret: function (_secret, _password) {
+            // TODO
+            if (_password == 'undefined' && typeof _password !== String) {
+                throw Error("Password must be set");
+            }
+            secret = _secret;
+            password = _password;
+        },
+        retrieveSecret: function (_password) {
+            // TODO
+            if (password === _password) {
+                return secret;
+            }
+            throw Error("Invalid password");
+        }
+    }
+    return safeBox;
+}
+var safeBox = passAdmin();
