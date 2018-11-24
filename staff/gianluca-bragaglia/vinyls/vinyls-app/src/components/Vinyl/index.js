bfd452043706511368b5d95bbf6feadfaad50ae5
Gianluca <gianluca.bragaglia@gmail.com>
2018-11-24 20:17:34 +0100

    more css

--- staff/gianluca-bragaglia/vinyls/vinyls-app/src/components/Vinyl/index.js
+++ staff/gianluca-bragaglia/vinyls/vinyls-app/src/components/Vinyl/index.js
@@ -3,38 +3,43 @@ import { withRouter } from 'react-router-dom'
 import AddComment from  '../AddComment'
 import CommentsList from  '../CommentsList'
 import Error from '../Error'
+import { Link } from 'react-router-dom'
 import logic from '../../logic'
 import './index.css'
 
 
 
 class Vinyl extends Component {
-    state = { title: '', artist: '', year: 0, info:'', imgVinylUrl: null, comments:[], likes:[], likeSelected: false, text:'', error: null, addComment: false }
+    state = { id: '', title: '', artist: '', year: 0, info:'', imgVinylUrl: null, comments:[], likes:[], likeSelected: false, text:'', error: null, addComment: false, user:{} }  
 
 
     componentDidMount() {
        
         try {
             logic.retrieveVinylById(this.props.id)
-            .then(vinyl => this.setState({ title: vinyl.title, artist: vinyl.artist, year: vinyl.year, info: vinyl.info, imgVinylUrl: vinyl.imgVinylUrl, comments: vinyl.comments, likes: vinyl.likes  }))
+            .then(vinyl => this.setState({ id: vinyl.id, title: vinyl.title, artist: vinyl.artist, year: vinyl.year, info: vinyl.info, imgVinylUrl: vinyl.imgVinylUrl, comments: vinyl.comments, likes: vinyl.likes  }))
             .catch(err => this.setState({ error: err.message }))
             logic.itsInLikes(this.props.id)        
             .then(res => this.setState ({ likeSelected: res }))
+            .then(() => {
+                logic.retrieveUserById(this.state.id)       
+                .then(user => this.setState ({ user }))
+                .catch(err => this.setState({ error: err.message }))
+            })
             .catch(err => this.setState({ error: err.message }))
         } catch (err) {
             this.setState({ error: err.message })
         }
     }
 
-    handleLikeClick = e => {
-  
+    handleLikeClick = e => { 
         e.preventDefault()
              try {       
             
                 logic.addLike(this.props.id)
                 .then(() => {
                     logic.retrieveVinylById(this.props.id)
-                    .then(vinyl => this.setState({ title: vinyl.title, artist: vinyl.artist, year: vinyl.year, info: vinyl.info, imgVinylUrl: vinyl.imgVinylUrl, comments: vinyl.comments, likes: vinyl.likes, likeSelected: true }))
+                    .then(vinyl => this.setState({ id: vinyl.id, title: vinyl.title, artist: vinyl.artist, year: vinyl.year, info: vinyl.info, imgVinylUrl: vinyl.imgVinylUrl, comments: vinyl.comments, likes: vinyl.likes, likeSelected: true }))
                 })
                 .catch(err => this.setState({ error: err.message }))
             } catch (err) {
@@ -51,7 +56,7 @@ class Vinyl extends Component {
                 logic.removeLike(this.props.id)
                 .then(() => {
                     logic.retrieveVinylById(this.props.id)
-                    .then(vinyl => { this.setState({ title: vinyl.title, artist: vinyl.artist, year: vinyl.year, info: vinyl.info, imgVinylUrl: vinyl.imgVinylUrl, comments: vinyl.comments, likes: vinyl.likes, likeSelected: false }) })
+                    .then(vinyl => { this.setState({  id: vinyl.id, title: vinyl.title, artist: vinyl.artist, year: vinyl.year, info: vinyl.info, imgVinylUrl: vinyl.imgVinylUrl, comments: vinyl.comments, likes: vinyl.likes, likeSelected: false }) })
                 })
                 .catch(err => this.setState({ error: err.message }))
             } catch (err) {
@@ -100,7 +105,8 @@ class Vinyl extends Component {
 
     render() {
 
-        const {error, imgVinylUrl, artist, title, year, info, likeSelected, likes, addComment } = this.state
+        const {error, imgVinylUrl, artist, title, year, info, likeSelected, likes, addComment, id, user } = this.state
+
 
         return  <div className='vinyl-container'>
                     <img className='vinyl-img'  src={imgVinylUrl ? imgVinylUrl : './img/vinyl.png'} ></img>
@@ -108,6 +114,8 @@ class Vinyl extends Component {
                     <p className='vinyl-title'>{title}</p>
                     <p className='vinyl-artist'>{artist}</p>
                     <p className='vinyl-year'>{year}</p>
+                    <Link to={`/profile/${id}`}><img className='profile-img-list' src={user.imgProfileUrl ? user.imgProfileUrl : './img/icon-profile.png'} ></img> <span className='profile-username-list'>{user.username}</span></Link>
+                    <br></br>
                     <a href="#" onClick={likeSelected ? this.handleDontLikeClick : this.handleLikeClick}> {likeSelected ? <span className='dont-like-btn'><i className='fa fa-star'><span className='likes-count'>{likes.length}</span></i></span> : <span className='like-btn'><i className='far fa-star'><span className='likes-count'>{likes.length}</span></i></span>}</a>
                     <p className='vinyl-info'>{info}</p>
                     <div className='msg-button'>
